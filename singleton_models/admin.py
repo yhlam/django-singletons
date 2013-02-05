2b6ac149356bfe2159f0340237ab675f910f59bb
Tim Heap <tim@ionata.com.au>
2013-02-05 14:53:46 +1100

    Fixing Django 1.5 admin, take two

--- singleton_models/admin.py
+++ singleton_models/admin.py
@@ -5,9 +5,22 @@ from django.http import HttpResponseRedirect
 from django.utils.functional import update_wrapper
 
 
+def singleton_view(fn_name):
+    def view(self, request, object_id, extra_context=None):
+        object_id = u'1'
+        self.model.objects.get_or_create(pk=1)
+
+        super_view = getattr(super(SingletonModelAdmin, self), fn_name)
+        return super_view(request, object_id, extra_context=extra_context)
+
+    view.__name__ = fn_name
+    return view
+
+
 class SingletonModelAdmin(admin.ModelAdmin):
 
     change_form_template = "admin/singleton_models/change_form.html"
+    object_history_template = "admin/singleton_models/object_history.html"
 
     def has_add_permission(self, request):
         """ Singleton pattern: prevent addition of new objects """
@@ -36,6 +49,9 @@ class SingletonModelAdmin(admin.ModelAdmin):
             url(r'^$',
                 wrap(self.change_view),
                 {u'object_id': 1},
+                name='%s_%s_change' % info),
+            url(r'^$',
+                wrap(self.changelist_view),
                 name='%s_%s_changelist' % info),
         )
 
@@ -55,12 +71,6 @@ class SingletonModelAdmin(admin.ModelAdmin):
             self.message_user(request, msg)
             return HttpResponseRedirect("../")
 
-    def change_view(self, request, object_id, extra_context=None):
-        if not isinstance(object_id, basestring):
-            object_id = unicode(object_id)
+    change_view = singleton_view('change_view')
 
-        if object_id == u'1':
-            self.model.objects.get_or_create(pk=1)
-
-        return super(SingletonModelAdmin, self).change_view(
-            request, object_id, extra_context)
+    history_view = singleton_view('history_view')
